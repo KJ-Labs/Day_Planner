1437117cd3395ba115e0bef42b423340078511d4
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-17 16:58:28 -0700

    Updated classes

--- script.js
+++ script.js
@@ -6,16 +6,11 @@ var currentContainer;
 var tempArray = [];
 var storedAppointments;
 var returnedAppointments;
-var textarea = document.getElementById("textarea");
+
 
 console.log(new Date().getHours())
 
 
-var t = new Date().getHours();
-if (t < 10) {
-  document.getElementById("textarea").style.backgroundColor = "lightblue";
-} else if (t < 20) {
-  document.getElementById("textarea").style.backgroundColor = "green";
-} else {
-    document.getElementById("textarea").style.backgroundColor = "red";
-}
+$(".textarea").each(function() {
+  $(this).addClass("future");
+})
