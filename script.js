e2b46e42f1b75b793e175dd7bcdb6827b5cb314e
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-18 14:19:54 -0700

    Adding local storage

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var appointTime = "";
 var currentContainer;
 let currenthour = new Date().getHours();
 
+
 console.log(currenthour);
 
 //past = grey, current = mint, future = blue
@@ -20,7 +21,19 @@ $(".textarea").each(function () {
 
 
 
+$(document).ready(function () {
+  var savesnotesbtn = document.getElementById("savebutton");
 
-$("container").click(function () {
-  console.log((this.attr('value')));
+  //FILL TEXT AREAS WITH NOTES
+  for (var i = 9; i <= 17; i++) {
+    $("#textarea" + i + "inputText").val(localStorage.getItem("textarea" + i));
+    console.log('button clicked?')
+  }
+
+  function saveNotes() {
+    for (var i = 1; i < 11; i++) {
+      localStorage.setItem("textarea" + i, $("#textarea" + i + "inputText").val());
+    }
+  };
+  savesnotesbtn.addEventListener("click", saveNotes);
 });
