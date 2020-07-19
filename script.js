0be729022c0d91a145f56e5b28c3322895d21517
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-18 19:43:15 -0700

    Added get to javascript

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ var containertime = $(".id").val();
 
 
 console.log(currenthour);
-
-//past = grey, current = mint, future = blue
-
 $(".textarea").each(function () {
   if (currenthour < $(this).attr("id") ) {
     $(this).addClass("future");
@@ -21,16 +18,28 @@ $(".textarea").each(function () {
     $(this).addClass("present");
   }
 })
+
 localStorage.clear();
 $(document).ready(function () {
   $(".saveBtn").on("click", function() {
     var textvalue = $(this).siblings('.inputText').find('textarea').val();
     var timevalue =  $(this).siblings('.inputText').find('textarea').attr("id");
     localStorage.setItem(timevalue, textvalue);
-    console.log(localStorage.getItem( '10'));
-    console.log(localStorage.getItem( '11'));
 
-    $(this).siblings('.inputText').find('textarea').val(localStorage.getItem( '10'));
+    console.log(localStorage.getItem('9'));
+    console.log(localStorage.getItem('10'));
+    console.log(localStorage.getItem('11'));
+    console.log(localStorage.getItem('12'));
+    console.log(localStorage.getItem('13'));
+    console.log(localStorage.getItem('14'));
+    console.log(localStorage.getItem('15'));
+    console.log(localStorage.getItem('16'));
+    console.log(localStorage.getItem('17'));
+
+
+    var timearray = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
+
   });
+
 });
 
