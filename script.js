56fc3eec2cdd2f1a1c4b141e1ff31b52bc1a709b
KJ-Labs <36465042+KJ-Labs@users.noreply.github.com>
2020-07-18 19:57:52 -0700

    Trying javascript loop

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ var appointText = "";
 var appointTime = "";
 var currentContainer;
 let currenthour = new Date().getHours();
+var timeblock9am = document.getElementById('.textarea#9');
+var timeblock10am = document.getElementById('.textarea#10');
+var timeblock11am = document.getElementById('.textarea#11');
+var timeblock12pm = document.getElementById('.textarea#12');
+var timeblock1pm = document.getElementById('.textarea#13');
+var timeblock2pm = document.getElementById('.textarea#14');
+var timeblock3pm = document.getElementById('.textarea#15');
+var timeblock4pm = document.getElementById('.textarea#16');
+var timeblock5pm = document.getElementById('.textarea#17');
+
+window.onbeforeunload = function(e) {
+  // Turning off the event
+  e.preventDefault();
+}
+
 
 var containervalue = document.getElementById('.ininputTextput');
 var containertime = $(".id").val();
@@ -36,10 +51,19 @@ $(document).ready(function () {
     console.log(localStorage.getItem('16'));
     console.log(localStorage.getItem('17'));
 
+    timeblock9am = localStorage.getItem('9');
 
-    var timearray = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
-
+    $(textvalue).each(function () {
+      if (textvalue.length = 0) {
+        0
+      }else {
+        textvalue = (localStorage.getItem($(this).siblings('.inputText').find('textarea').attr("id")))
+      }
   });
 
 });
+
+});
+
+
 
